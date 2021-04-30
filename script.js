b4f01891a245f2366449ea205c46db30bba1e6b1
Jason Wood <jgw10713@gmail.com>
2021-04-29 20:06:29 -0700

    OK

---
+++ script.js
@@ -0,0 +1,30 @@
+// // api url
+// const api_url = 
+//       "https://api.themoviedb.org/3/discover/movie?api_key=2d68f36569896b3eca3f4d442ec3c9a3&language=en-US&with_genres=878&primary_release_year=2020";
+  
+// // Defining async function
+// async function getapi(url) {
+    
+//     // Storing response
+//     const response = await fetch(url);
+    
+//     // Storing data in form of JSON
+//     var data = await response.json();
+//     console.log(data);
+//     if (response) {
+//         hideloader();
+//     }
+//     show(data);
+// }
+// // Calling that async function
+// getapi(api_url);
+  
+// // Function to hide the loader
+// function hideloader() {
+//     document.getElementById('loading').style.display = 'none';
+// }
+
+
+
+
+  
